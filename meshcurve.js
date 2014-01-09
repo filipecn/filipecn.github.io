8c4f85882874fca5e29dac34ecc5d90577b7b42f
filipecn <fuiripecn@gmail.com>
2014-01-09 12:19:45 -0200

    ply loader

--- meshcurve.js
+++ meshcurve.js
@@ -131,42 +131,86 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 } else {
 	alert('The File APIs are not fully supported in this browser.');
 }
-var f;
-function handleFileSelect(evt) {
- 	var files = evt.target.files; // FileList object
 
- 	// files is a FileList of File objects. List some properties.
- 	var output = [];
- 	for (var i = 0; f = files[i]; i++) {
- 		output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-   		f.size, ' bytes, last modified: ',
-   		f.lastModifiedDate.toLocaleDateString(), '</li>');
 
- 		if (f) {
-      		var r = new FileReader();
-      		r.onload = function(e) { 
-	      		var contents = e.target.result;
-        		alert( 
-        			"Got the file.n" 
-             		/*+"name: " + f.name + "n"
-              		+"type: " + f.type + "n"
-              		+"size: " + f.size + " bytesn"*/
-              		+ "starts with: " + contents.substr(1, contents.indexOf("n"))
-        		
-        		);  
+  var reader;
+  var progress = document.querySelector('.percent');
 
-        		console.log(contents);
-      		}
-      		r.readAsText(f);
-    	} else { 
-      		alert("Failed to load file");
-    	}
-   	}
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-}
+  function abortRead() {
+    reader.abort();
+  }
 
-document.getElementById('files').addEventListener('change', handleFileSelect, false);
+  function errorHandler(evt) {
+    switch(evt.target.error.code) {
+      case evt.target.error.NOT_FOUND_ERR:
+        alert('File Not Found!');
+        break;
+      case evt.target.error.NOT_READABLE_ERR:
+        alert('File is not readable');
+        break;
+      case evt.target.error.ABORT_ERR:
+        break; // noop
+      default:
+        alert('An error occurred reading this file.');
+    };
+  }
 
+  function updateProgress(evt) {
+    // evt is an ProgressEvent.
+    if (evt.lengthComputable) {
+      var percentLoaded = Math.round((evt.loaded / evt.total) * 100);
+      // Increase the progress bar length.
+      if (percentLoaded < 100) {
+        progress.style.width = percentLoaded + '%';
+        progress.textContent = percentLoaded + '%';
+      }
+    }
+  }
+
+  function handleFileSelect(evt) {
+    // Reset progress indicator on new file selection.
+    progress.style.width = '0%';
+    progress.textContent = '0%';
+
+    reader = new FileReader();
+    reader.onerror = errorHandler;
+    reader.onprogress = updateProgress;
+    reader.onabort = function(e) {
+      alert('File read cancelled');
+    };
+    reader.onloadstart = function(e) {
+      document.getElementById('progress_bar').className = 'loading';
+    };
+    reader.onload = function(e) {
+      // Ensure that the progress bar displays 100% at the end.
+      progress.style.width = '100%';
+      progress.textContent = '100%';
+      setTimeout("document.getElementById('progress_bar').className='';", 2000);
+
+
+
+      var contents = e.target.result;
+
+        		var loader = new THREE.PLYLoader();
+				var geometry = loader.parse(contents);
+				var material = new THREE.MeshPhongMaterial( { ambient: 0x0055ff, color: 0x0055ff, specular: 0x111111, shininess: 200 } );
+					var mesh = new THREE.Mesh( geometry, material );
+
+					mesh.position.set( 0, - 0.25, 0 );
+					mesh.rotation.set( 0, - Math.PI / 2, 0 );
+					mesh.scale.set( 0.001, 0.001, 0.001 );
+
+					mesh.castShadow = true;
+					mesh.receiveShadow = true;
+
+					scene.add( mesh );
+
+    }
+	
+	reader.readAsText(evt.target.files[0]);
+  }
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
 var noise=new ImprovedNoise();
 var isSafari=/Safari/.test(navigator.userAgent)&&/Apple Computer/.test(navigator.vendor);
@@ -304,7 +348,7 @@ function loadSuzanne(callback){
 		geometry.faces.push(f);
 		var uvs=[];
 		var ids=[ 'a','b','c','d'];
-		for(var i=0;i<ids.length;i++){
+		for(var i=0;i<3/*ids.length*/;i++){
 			var vertex=geometry.vertices[f[ids[i]]].clone();
 			var n=vertex.normalize();
 			var yaw=.5-Math.atan(n.z,-n.x)/(2.0*Math.PI);
@@ -409,7 +453,7 @@ function init(){
 	window.addEventListener('resize',onWindowResize,false);
 	onWindowResize();
 	clearMesh();
-	loadSuzanne(addGeometry);
+	//loadSuzanne(addGeometry);
 	render();
 }
 /*
